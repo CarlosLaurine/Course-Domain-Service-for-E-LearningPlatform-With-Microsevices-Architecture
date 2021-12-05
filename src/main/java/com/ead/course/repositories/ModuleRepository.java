4631cb9b7c00e45527269d83af67e4164f3ef426
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2021-12-05 01:31:52 -0300

    @EntityGraph Query for EAGERLY Loading the respective Course Attribute while Finding a Module By Title + @Query Native Query for Finding All Modules By Course Id Implemented at ModuleRepoitory

--- src/main/java/com/ead/course/repositories/ModuleRepository.java
+++ src/main/java/com/ead/course/repositories/ModuleRepository.java
@@ -2,9 +2,19 @@ package com.ead.course.repositories;
 
 import com.ead.course.models.CourseModel;
 import com.ead.course.models.ModuleModel;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ModuleRepository extends JpaRepository<ModuleModel, UUID> {
+
+    @Query(value = "select * from tb_modules where course_course_id = :courseId", nativeQuery = true)
+    List<ModuleModel> findAllModulesIntoCourse(@Param("courseId") UUID courseId);
+
+    @EntityGraph(attributePaths = {"course"})
+    ModuleModel findByTitle(String title);
 }
