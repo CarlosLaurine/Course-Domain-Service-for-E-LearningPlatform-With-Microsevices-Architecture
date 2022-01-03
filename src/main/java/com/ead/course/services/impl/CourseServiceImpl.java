0da4f9b7a04bdde4fd07a5f35cf877e6e79d1c12
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2022-01-03 20:00:12 -0300

    Cascading Course Deletion Logic + Method Implemented at CourseService Interface and Impl Classes

--- src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -1,12 +1,41 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.models.CourseModel;
+import com.ead.course.models.LessonModel;
+import com.ead.course.models.ModuleModel;
 import com.ead.course.repositories.CourseRepository;
+import com.ead.course.repositories.LessonRepository;
+import com.ead.course.repositories.ModuleRepository;
 import com.ead.course.services.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 public class CourseServiceImpl implements CourseService {
     @Autowired
     private CourseRepository courseRepository;
+    @Autowired
+    private ModuleRepository moduleRepository;
+    @Autowired
+    private LessonRepository lessonRepository;
+
+    @Transactional
+    @Override
+    public void delete(CourseModel courseModel) {
+
+        List<ModuleModel> courseModulesList = moduleRepository.findAllModulesIntoCourse(courseModel.getCourseId());
+        if(!courseModulesList.isEmpty()){
+            for(ModuleModel module : courseModulesList){
+                List<LessonModel> moduleLessonsList = lessonRepository.findAllLessonsIntoModule(module.getModuleId());
+                if(!moduleLessonsList.isEmpty()){
+                    lessonRepository.deleteAll(moduleLessonsList);
+                }
+            }
+            moduleRepository.deleteAll(courseModulesList);
+        }
+        courseRepository.delete(courseModel);
+    }
 }
