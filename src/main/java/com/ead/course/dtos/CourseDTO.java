5b26b9836597edfeccb369e4d38d225dfa1d7098
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2022-01-04 23:26:52 -0300

    DTOs Package + CourseDTO Created

---
+++ src/main/java/com/ead/course/dtos/CourseDTO.java
@@ -0,0 +1,26 @@
+package com.ead.course.dtos;
+
+import com.ead.course.enums.CourseLevel;
+import com.ead.course.enums.CourseStatus;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+@Data
+public class CourseDTO {
+
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String description;
+    private String imgURL;
+    @NotNull
+    private CourseStatus courseStatus;
+    @NotNull
+    private UUID userInstructorId;
+    @NotNull
+    private CourseLevel courseLevel;
+
+}
