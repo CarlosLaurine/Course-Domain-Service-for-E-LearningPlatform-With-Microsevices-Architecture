d27532586188b0b4b20ae006f840cb46d2fdfeb9
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2022-01-06 20:49:02 -0300

    LessonService + Impl Configured with Methods findById + findAll + delete + save

--- src/main/java/com/ead/course/services/impl/LessonServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/LessonServiceImpl.java
@@ -1,12 +1,37 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.models.LessonModel;
 import com.ead.course.repositories.LessonRepository;
 import com.ead.course.services.LessonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 public class LessonServiceImpl implements LessonService {
     @Autowired
     private LessonRepository lessonRepository;
+
+    @Override
+    public LessonModel save(LessonModel lessonModel) {
+        return lessonRepository.save(lessonModel);
+    }
+
+    @Override
+    public Optional<LessonModel> findLessonIntoModuleById(UUID moduleId, UUID lessonId) {
+        return lessonRepository.findLessonIntoModuleById(moduleId, lessonId);
+    }
+
+    @Override
+    public void delete(LessonModel lessonModel) {
+        lessonRepository.delete(lessonModel);
+    }
+
+    @Override
+    public List<LessonModel> findAllLessonsByModule(UUID moduleId) {
+        return lessonRepository.findAllLessonsIntoModule(moduleId);
+    }
 }
