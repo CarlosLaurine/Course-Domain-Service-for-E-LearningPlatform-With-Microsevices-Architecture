019b63907cbc7a2f821ad2cb73077a18d5b00ca1
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2022-01-06 20:51:55 -0300

    LessonRepository Configured with findLessonIntoModuleById Custom Query Method

--- src/main/java/com/ead/course/services/impl/ModuleServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/ModuleServiceImpl.java
@@ -45,4 +45,9 @@ public class ModuleServiceImpl implements ModuleService {
     public List<ModuleModel> findAllModulesByCourse(UUID courseId) {
         return moduleRepository.findAllModulesIntoCourse(courseId);
     }
+
+    @Override
+    public Optional<ModuleModel> findById(UUID moduleId) {
+        return moduleRepository.findById(moduleId);
+    }
 }
