ffb92ab184373bcd1616d811b8bdd5d68a4f2fbd
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2022-01-05 21:32:20 -0300

    ModuleRepository Configured with findModuleIntoCourseById Method

--- src/main/java/com/ead/course/repositories/ModuleRepository.java
+++ src/main/java/com/ead/course/repositories/ModuleRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ModuleRepository extends JpaRepository<ModuleModel, UUID> {
@@ -17,4 +18,7 @@ public interface ModuleRepository extends JpaRepository<ModuleModel, UUID> {
 
     @EntityGraph(attributePaths = {"course"})
     ModuleModel findByTitle(String title);
+
+    @Query(value = "select * from tb_modules where course_course_id = :courseId and module_id = :moduleId", nativeQuery = true)
+    Optional<ModuleModel> findModuleIntoCourseById(@Param("courseId") UUID courseId, @Param("moduleId") UUID moduleId);
 }
