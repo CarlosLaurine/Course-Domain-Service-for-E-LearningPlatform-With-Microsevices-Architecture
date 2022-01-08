d6b57e50d60a6c6cb17c5ea0b218fe19b59e1695
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2022-01-07 22:30:55 -0300

    JpaSpecificationExecutor Extension Added to CourseRepository

--- src/main/java/com/ead/course/repositories/CourseRepository.java
+++ src/main/java/com/ead/course/repositories/CourseRepository.java
@@ -1,9 +1,15 @@
 package com.ead.course.repositories;
 
 import com.ead.course.models.CourseModel;
+import com.ead.course.specifications.SpecificationTemplate;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
 import java.util.UUID;
 
-public interface CourseRepository extends JpaRepository<CourseModel, UUID> {
+public interface CourseRepository extends JpaRepository<CourseModel, UUID>, JpaSpecificationExecutor<CourseModel> {
+    Page<CourseModel> findAll(SpecificationTemplate.CourseSpec spec, Pageable pageable);
 }
