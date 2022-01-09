de29cf77522966d4bfccf495a83e5fffd0e69e39
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2022-01-08 21:51:36 -0300

    moduleCourseId Added to SpecificationTemplate to set CriteriaBuilder to Join Table Query for Course-related Modules

--- src/main/java/com/ead/course/controllers/ModuleController.java
+++ src/main/java/com/ead/course/controllers/ModuleController.java
@@ -5,8 +5,12 @@ import com.ead.course.models.CourseModel;
 import com.ead.course.models.ModuleModel;
 import com.ead.course.services.CourseService;
 import com.ead.course.services.ModuleService;
+import com.ead.course.specifications.SpecificationTemplate;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -66,7 +70,9 @@ public class ModuleController {
     }
 
     @GetMapping("/courses/{courseId}/modules")
-    public ResponseEntity<List<ModuleModel>> getAllModulesFromACourse(@PathVariable(value = "courseId") UUID courseId){
+    public ResponseEntity<List<ModuleModel>> getAllModulesFromACourse(@PathVariable(value = "courseId") UUID courseId,
+                                                                      SpecificationTemplate.ModuleSpec spec,
+                                                                      @PageableDefault(page = 0, size = 10, sort = "moduleId", direction = Sort.Direction.ASC) Pageable pageable){
         return ResponseEntity.status(HttpStatus.OK).body(moduleService.findAllModulesByCourse(courseId));
     }
 
