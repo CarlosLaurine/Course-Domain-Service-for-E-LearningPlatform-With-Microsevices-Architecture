cdfc4bd39badaad53417f62905aafcdbc44d3318
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2022-01-21 23:35:50 -0300

    RestTemplate Config Class with Bean Created at Project

---
+++ src/main/java/com/ead/course/configs/RestTemplateConfig.java
@@ -0,0 +1,16 @@
+package com.ead.course.configs;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        // Additional Configuration for Custom Exception Handling can be placed here
+        return builder.build();
+    }
+}
