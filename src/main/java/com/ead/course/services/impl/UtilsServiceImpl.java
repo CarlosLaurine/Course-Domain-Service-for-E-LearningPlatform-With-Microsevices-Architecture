5bf4d6a74f5534fb098897e6a14e7a548e4487ab
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2022-01-21 23:34:55 -0300

    API Composition Internal Request URI Generation Externalized with UtilsService Interface and Implementation

---
+++ src/main/java/com/ead/course/services/impl/UtilsServiceImpl.java
@@ -0,0 +1,19 @@
+package com.ead.course.services.impl;
+
+import com.ead.course.services.UtilsService;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class UtilsServiceImpl implements UtilsService {
+
+    private String REQUEST_URI = "http://localhost:8082";
+
+    @Override
+    public String generateUrl(UUID courseId, Pageable pageable) {
+        return REQUEST_URI + "/users?courseId=" + courseId + "&page=" + pageable.getPageNumber() + "&size="
+                + pageable.getPageSize() + "&sort=" + pageable.getSort().toString().replaceAll(":", ",");
+    }
+}
