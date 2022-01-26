a54dedcd05015bbdf4256bef1be23bdf50c2c013
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2022-01-26 02:53:20 -0300

    User Status/Existence Validations with External API Communication Iimplemented at registerSubscriptionUserInCourse() Method at CourseUserController

--- src/main/java/com/ead/course/controllers/CourseUserController.java
+++ src/main/java/com/ead/course/controllers/CourseUserController.java
@@ -3,6 +3,7 @@ package com.ead.course.controllers;
 import com.ead.course.clients.AuthUserClient;
 import com.ead.course.dtos.SubscriptionDTO;
 import com.ead.course.dtos.UserDTO;
+import com.ead.course.enums.UserStatus;
 import com.ead.course.models.CourseModel;
 import com.ead.course.models.CourseUserModel;
 import com.ead.course.services.CourseService;
@@ -16,6 +17,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpStatusCodeException;
 
 import javax.validation.Valid;
 import java.util.Optional;
@@ -27,7 +29,7 @@ import java.util.UUID;
 public class CourseUserController {
 
     @Autowired
-    private AuthUserClient userClient;
+    private AuthUserClient authUserClient;
 
     @Autowired
     private CourseService courseService;
@@ -38,24 +40,46 @@ public class CourseUserController {
     @GetMapping("/courses/{courseId}/users")
     public ResponseEntity<Page<UserDTO>> getAllUsersByCourse(@PathVariable(value = "courseId") UUID courseId,
                                                              @PageableDefault(page = 0, size = 10, sort = "userId", direction = Sort.Direction.ASC) Pageable pageable){
-        return ResponseEntity.status(HttpStatus.OK).body(userClient.getAllUsersByCourse(courseId, pageable));
+        return ResponseEntity.status(HttpStatus.OK).body(authUserClient.getAllUsersByCourse(courseId, pageable));
     }
 
     @PostMapping("/courses/{courseId}/users/subscription")
     public ResponseEntity<Object> registerSubscriptionUserInCourse(@PathVariable(value = "courseId") UUID courseId,
                                                                    @RequestBody @Valid SubscriptionDTO dto){
+
+        ResponseEntity<UserDTO> responseUser;
+
         Optional<CourseModel> courseModelOptional = courseService.findById(courseId);
+
         if(!courseModelOptional.isPresent()){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course Not Found");
         }
 
         if(courseUserService.existsByCourseAndUserId(courseModelOptional.get(), dto.getUserId())){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already registered for this Course");
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("User already registered for this Course");
+        }
+
+        try {
+
+            responseUser = authUserClient.getUserById(dto.getUserId());
+
+            if(responseUser.getBody().getUserStatus().equals(UserStatus.BLOCKED)){
+                return ResponseEntity.status(HttpStatus.CONFLICT).body("User is Blocked");
+            }
+
+        }
+
+        catch (HttpStatusCodeException exception) {
+
+            if(exception.getStatusCode().equals(HttpStatus.NOT_FOUND)){
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+            }
+
         }
 
         CourseUserModel courseUserModel = courseUserService.save(courseModelOptional.get().convertToCourseUserModel(dto.getUserId()));
 
-        return ResponseEntity.status(HttpStatus.CREATED).body("Subscription successfully created");
+        return ResponseEntity.status(HttpStatus.CREATED).body(courseUserModel);
 
     }
 
