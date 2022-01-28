02efba7bb8b861ffe6ed123d956b9cbd68dcd302
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2022-01-28 01:28:14 -0300

    Atomic Multi-Transactional Method saveAndSendSubscriptionUserInCourse() Created at CourseUserService Interface + Implementation

--- src/main/java/com/ead/course/services/impl/CourseUserServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/CourseUserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.clients.AuthUserClient;
 import com.ead.course.models.CourseModel;
 import com.ead.course.models.CourseUserModel;
 import com.ead.course.repositories.CourseUserRepository;
@@ -7,12 +8,17 @@ import com.ead.course.services.CourseUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.UUID;
 
 @Service
 public class CourseUserServiceImpl implements CourseUserService {
+
     @Autowired
     private CourseUserRepository courseUserRepository;
+
+    @Autowired
+    private AuthUserClient authUserClient;
 
     @Override
     public boolean existsByCourseAndUserId(CourseModel courseModel, UUID userId) {
@@ -23,4 +29,16 @@ public class CourseUserServiceImpl implements CourseUserService {
     public CourseUserModel save(CourseUserModel courseUserModel) {
         return courseUserRepository.save(courseUserModel);
     }
+
+    @Transactional
+    @Override
+    public CourseUserModel saveAndSendSubscriptionUserInCourse(CourseUserModel courseUserModel) {
+
+        courseUserModel = courseUserRepository.save(courseUserModel);
+
+        authUserClient.postSubscriptionUserInCourse(courseUserModel.getUserId(), courseUserModel.getCourse().getCourseId());
+
+        return courseUserModel;
+    }
+
 }
