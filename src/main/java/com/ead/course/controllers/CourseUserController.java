e337624574519deb57cda7d2b5677d61239860a6
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2022-01-28 01:29:48 -0300

    Authuser Microsservice Database Update with External API Communication Implemented at registerSubscriptionUserInCourse() Method at CourseUserController

--- src/main/java/com/ead/course/controllers/CourseUserController.java
+++ src/main/java/com/ead/course/controllers/CourseUserController.java
@@ -77,7 +77,7 @@ public class CourseUserController {
 
         }
 
-        CourseUserModel courseUserModel = courseUserService.save(courseModelOptional.get().convertToCourseUserModel(dto.getUserId()));
+        CourseUserModel courseUserModel = courseUserService.saveAndSendSubscriptionUserInCourse(courseModelOptional.get().convertToCourseUserModel(dto.getUserId()));
 
         return ResponseEntity.status(HttpStatus.CREATED).body(courseUserModel);
 
