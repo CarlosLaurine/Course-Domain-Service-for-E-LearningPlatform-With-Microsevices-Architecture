e154f854cce56d97528445af036f1dbd46d3bb0d
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2022-01-30 23:14:11 -0300

    Log4J2 Debug+Info+Error Level Logs Full Coverage Implemented at ModuleController

--- src/main/java/com/ead/course/controllers/ModuleController.java
+++ src/main/java/com/ead/course/controllers/ModuleController.java
@@ -6,6 +6,7 @@ import com.ead.course.models.ModuleModel;
 import com.ead.course.services.CourseService;
 import com.ead.course.services.ModuleService;
 import com.ead.course.specifications.SpecificationTemplate;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -23,9 +24,11 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Log4j2
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class ModuleController {
+
     @Autowired
     private ModuleService moduleService;
     @Autowired
@@ -34,25 +37,47 @@ public class ModuleController {
     @PostMapping("/courses/{courseId}/modules")
     public ResponseEntity<Object> insertModule( @PathVariable(value = "courseId") UUID courseId,
                                                 @RequestBody @Valid ModuleDTO moduleDTO){
+
+        log.debug("POST saveModule moduleDto received {} ", moduleDTO.toString());
+
         Optional<CourseModel> courseModelOptional = courseService.findById(courseId);
+
         if(!courseModelOptional.isPresent()){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course Not Found");
         }
+
         var moduleModel = new ModuleModel();
+
         BeanUtils.copyProperties(moduleDTO, moduleModel);
+
         moduleModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
         moduleModel.setCourse(courseModelOptional.get());
-        return ResponseEntity.status(HttpStatus.CREATED).body(moduleService.save(moduleModel));
+
+        moduleService.save(moduleModel);
+
+        log.debug("POST saveModule moduleId saved {} ", moduleModel.getModuleId());
+        log.info("Module saved successfully moduleId {} ", moduleModel.getModuleId());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(moduleModel);
     }
 
     @DeleteMapping("/courses/{courseId}/modules/{moduleId}")
     public ResponseEntity<Object> deleteModule(@PathVariable(value = "courseId") UUID courseId,
                                                @PathVariable(value = "moduleId") UUID moduleId){
+
+        log.debug("DELETE deleteModule moduleId received {} ", moduleId);
+
         Optional<ModuleModel> moduleModelOptional = moduleService.findModuleIntoCourseById(courseId, moduleId);
+
         if(!moduleModelOptional.isPresent()){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Module Not Found for this Course");
         }
+
         moduleService.delete(moduleModelOptional.get());
+
+        log.debug("DELETE deleteModule moduleId deleted {} ", moduleId);
+        log.info("Module deleted successfully moduleId {} ", moduleId);
+
         return ResponseEntity.status(HttpStatus.OK).body("Module Deleted Successfully");
     }
 
@@ -60,14 +85,24 @@ public class ModuleController {
     public ResponseEntity<Object> updateModule( @RequestBody @Valid ModuleDTO moduleDTO,
                                                 @PathVariable(value = "courseId") UUID courseId,
                                                 @PathVariable(value = "moduleId") UUID moduleId){
+        log.debug("PUT updateModule moduleDto received {} ", moduleDTO.toString());
+
         Optional<ModuleModel> moduleModelOptional = moduleService.findModuleIntoCourseById(courseId, moduleId);
+
         if(!moduleModelOptional.isPresent()){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Module Not Found for this Course");
         }
+
         var moduleModel = moduleModelOptional.get();
+
         BeanUtils.copyProperties(moduleDTO, moduleModel);
 
-        return ResponseEntity.status(HttpStatus.OK).body(moduleService.save(moduleModel));
+        moduleService.save(moduleModel);
+
+        log.debug("PUT updateModule moduleId saved {} ", moduleModel.getModuleId());
+        log.info("Module updated successfully moduleId {} ", moduleModel.getModuleId());
+
+        return ResponseEntity.status(HttpStatus.OK).body(moduleModel);
     }
 
     @GetMapping("/courses/{courseId}/modules")
