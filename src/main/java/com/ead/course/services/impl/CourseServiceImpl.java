f5173e257840ec8c3abf32242196d5c7ba084934
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2022-02-27 23:45:30 -0300

    CourseServiceImpl Updated for Asynchronous Communication Implementation

--- src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -1,14 +1,12 @@
 package com.ead.course.services.impl;
 
-import com.ead.course.clients.AuthUserClient;
 import com.ead.course.models.CourseModel;
-import com.ead.course.models.CourseUserModel;
 import com.ead.course.models.LessonModel;
 import com.ead.course.models.ModuleModel;
 import com.ead.course.repositories.CourseRepository;
-import com.ead.course.repositories.CourseUserRepository;
 import com.ead.course.repositories.LessonRepository;
 import com.ead.course.repositories.ModuleRepository;
+import com.ead.course.repositories.UserRepository;
 import com.ead.course.services.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -30,15 +28,11 @@ public class CourseServiceImpl implements CourseService {
     @Autowired
     private LessonRepository lessonRepository;
     @Autowired
-    private CourseUserRepository courseUserRepository;
-    @Autowired
-    private AuthUserClient authUserClient;
+    private UserRepository userRepository;
 
     @Transactional
     @Override
     public void delete(CourseModel courseModel) {
-
-        boolean userCourseBindingsExist = false;
 
         List<ModuleModel> courseModulesList = moduleRepository.findAllModulesIntoCourse(courseModel.getCourseId());
         if(!courseModulesList.isEmpty()){
@@ -50,15 +44,9 @@ public class CourseServiceImpl implements CourseService {
             }
             moduleRepository.deleteAll(courseModulesList);
         }
-        List<CourseUserModel> subscriptions = courseUserRepository.findAllSubscriptionsFromACourse(courseModel.getCourseId());
-        if(!subscriptions.isEmpty()){
-            courseUserRepository.deleteAll(subscriptions);
-            userCourseBindingsExist = true;
-        }
+
         courseRepository.delete(courseModel);
-        if(userCourseBindingsExist){
-            authUserClient.deleteCourseInAuthUser(courseModel.getCourseId());
-        }
+
     }
 
     @Override
