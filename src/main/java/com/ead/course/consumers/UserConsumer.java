cc0a736f7d3b3f6a6916a3081932c032b3520c17
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2022-03-10 00:19:52 -0300

    Component UserConsumer Configured with Rabbit Listener Method listenUserEvent() + Payload Persistence Logic Implemented

---
+++ src/main/java/com/ead/course/consumers/UserConsumer.java
@@ -0,0 +1,34 @@
+package com.ead.course.consumers;
+
+import com.ead.course.dtos.UserEventDTO;
+import com.ead.course.enums.ActionType;
+import com.ead.course.services.UserService;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserConsumer {
+
+    @Autowired
+    private UserService userService;
+
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(value = "${ead.broker.queue.user-event-queue.name}", durable = "true"),
+            exchange = @Exchange(value = "${ead.broker.exchange.user-event-exchange}", type = ExchangeTypes.FANOUT, ignoreDeclarationExceptions = "true"))
+    )
+    public void listenUserEvent(@Payload UserEventDTO userEventDTO){
+        var userModel = userEventDTO.convertToUserModel();
+
+        switch (ActionType.valueOf(userEventDTO.getActionType())){
+            case CREATE:
+                userService.save(userModel);
+                break;
+        }
+    }
+}
