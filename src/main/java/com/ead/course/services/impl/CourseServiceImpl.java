55fb4aec65ac1eba20814fc12685364a0a5edfda
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2022-04-12 22:45:24 -0300

    Method saveSubscriptionUserInCourseAndSendNotification Configured at CourseService Implementation with Log4J2

--- src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -1,13 +1,17 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.dtos.NotificationCommandDTO;
 import com.ead.course.models.CourseModel;
 import com.ead.course.models.LessonModel;
 import com.ead.course.models.ModuleModel;
+import com.ead.course.models.UserModel;
+import com.ead.course.publishers.NotificationCommandPublisher;
 import com.ead.course.repositories.CourseRepository;
 import com.ead.course.repositories.LessonRepository;
 import com.ead.course.repositories.ModuleRepository;
 import com.ead.course.repositories.UserRepository;
 import com.ead.course.services.CourseService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,8 +23,10 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Log4j2
 @Service
 public class CourseServiceImpl implements CourseService {
+
     @Autowired
     private CourseRepository courseRepository;
     @Autowired
@@ -29,6 +35,8 @@ public class CourseServiceImpl implements CourseService {
     private LessonRepository lessonRepository;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private NotificationCommandPublisher notificationCommandPublisher;
 
     @Transactional
     @Override
@@ -68,9 +76,26 @@ public class CourseServiceImpl implements CourseService {
     public boolean existsByCourseAndUser(UUID courseId, UUID userId) {
         return courseRepository.existsByCourseAndUser(courseId, userId);
     }
+
     @Transactional
     @Override
     public void saveSubscriptionUserInCourse(UUID courseId, UUID userId) {
         courseRepository.saveCourseUser(courseId, userId);
     }
+
+    @Transactional
+    @Override
+    public void saveSubscriptionUserInCourseAndSendNotification(CourseModel course, UserModel user) {
+        courseRepository.saveCourseUser(course.getCourseId(), user.getUserId());
+        try {
+            var notificationCommandDto = new NotificationCommandDTO();
+            notificationCommandDto.setTitle("Welcome to Course: " + course.getName());
+            notificationCommandDto.setMessage(user.getFullName() + " your subscription was successfully made!");
+            notificationCommandDto.setUserId(user.getUserId());
+            notificationCommandPublisher.publishNotificationCommand(notificationCommandDto);
+        } catch (Exception e) {
+            log.warn("Error sending notification about subscription for userId -> {}", user.getUserId());
+        }
+    }
+
 }
