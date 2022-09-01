e362373a152f5e550291b4f5fd52175bb31b0736
Carlos L. Pinho <carlos.pinho@ucsal.edu.com.br>
2022-08-31 22:06:05 -0300

    CourseRepository Refactored

--- src/main/java/com/ead/course/repositories/CourseRepository.java
+++ src/main/java/com/ead/course/repositories/CourseRepository.java
@@ -1,20 +1,15 @@
 package com.ead.course.repositories;
 
 import com.ead.course.models.CourseModel;
-import com.ead.course.specifications.SpecificationTemplate;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface CourseRepository extends JpaRepository<CourseModel, UUID>, JpaSpecificationExecutor<CourseModel> {
-    Page<CourseModel> findAll(SpecificationTemplate.CourseSpec spec, Pageable pageable);
 
     @Query(value="select case when count(tcu) > 0 THEN true ELSE false END FROM tb_courses_users tcu WHERE tcu.course_id= :courseId and tcu.user_id= :userId",nativeQuery = true)
     boolean existsByCourseAndUser(@Param("courseId") UUID courseId, @Param("userId") UUID userId);
